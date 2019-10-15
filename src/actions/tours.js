a3aa5d5f5bffa887aace9e7fa0fbcaffa495c429
magic-onion <alexander.m.truong@gmail.com>
2019-10-15 11:37:05 -0400

    edit function going well

--- src/actions/tours.js
+++ src/actions/tours.js
@@ -128,7 +128,7 @@ export const editTourPatchReq = (tourObj, param) => {
     let fetchString = `http://localhost:3000/api/v1/tours/${param}`
     console.log(fetchString, config, 'editTourPatch')
     fetch(fetchString, config)
-      .then(r=>r.json)
-      .then(p=>console.log)
+      .then(r=>r.json())
+      .then(console.log)
   }
 }
