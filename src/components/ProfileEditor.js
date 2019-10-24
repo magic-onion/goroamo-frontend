da2284911689732e5c4b7c296ce0a1f21d492f29
magic-onion <alexander.m.truong@gmail.com>
2019-10-24 15:45:45 -0500

    profile editing

--- src/components/ProfileEditor.js
+++ src/components/ProfileEditor.js
@@ -8,7 +8,7 @@ class ProfileEditor extends React.Component {
     this.state = {
       first_name: props.user.first_name,
       last_name: props.user.last_name,
-      locattion: props.user.location,
+      location: props.user.location,
       email: props.user.email,
       avatar: props.user.avatar,
       password: "",
@@ -23,16 +23,41 @@ class ProfileEditor extends React.Component {
     this.setState({[e.target.id]: e.target.value})
   }
 
+  editUser() {
+    let userObj = {
+      user: {
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        location: this.state.location,
+        email: this.state.email,
+        avatar: this.state.avatar,
+        password: this.state.password
+      }
+    }
+  }
+
   get editForm() {
     return (
       <div className="profile-editor">
+        <span>first name:</span>
         <input type="text" id="first_name" onChange={e=>this.handleChange(e)} value={this.state.first_name}/>
+
+        <span>last name:</span>
         <input type="text" id="last_name" onChange={e=>this.handleChange(e)} value={this.state.last_name}/>
+
+        <span>email:</span>
         <input type="text" id="email" onChange={e=>this.handleChange(e)} value={this.state.email}/>
+
+        <span>location:</span>
         <input type="text" id="location" onChange={e=>this.handleChange(e)} value={this.state.location}/>
         <p>New Password</p>
-        <input type="text" id="password" onChange={e=>this.handleChange(e)} value={this.state.pasword}/>
+        <span>password</span>
+        <input type="text" id="password" onChange={e=>this.handleChange(e)} value={this.state.password}/>
+
+        <span>confirm password:</span>
         <input type="text" id="passwordVerify" onChange={e=>this.handleChange(e)} value={this.state.passwordVerify}/>
+
+        <button>Save changes</button>
       </div>
     )
   }
--- src/containers/ProfileViewer.js
+++ src/containers/ProfileViewer.js
@@ -5,6 +5,20 @@ import ProfileWidget from '../components/ProfileWidget'
 import ProfileEditor from '../components/ProfileEditor'
 
 class ProfileViewer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editing: false
+    }
+
+    this.toggleEditing = this.toggleEditing.bind(this)
+  }
+
+  toggleEditing(e) {
+    let editState = !this.state.editing
+    this.setState({editing: editState})
+  }
 
   get profileInfo() {
     if (this.props.user.user.id === undefined) {
@@ -12,9 +26,8 @@ class ProfileViewer extends React.Component {
     }
     else return (
       <div className="profile-viewer">
-        <button>edit my profile</button>
-        <ProfileWidget/>
-        <ProfileEditor user={this.props.user.user}/>
+        <button onClick={e=>this.toggleEditing(e)}>{this.state.editing ? "go back" : "edit my profile"}</button>
+        {this.state.editing ? <ProfileEditor user={this.props.user.user}/> : <ProfileWidget/>}
       </div>
     )
   }
