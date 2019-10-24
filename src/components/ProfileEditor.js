dd44fbf066ef41c9995bd444da446d893a01e480
magic-onion <alexander.m.truong@gmail.com>
2019-10-24 15:33:00 -0500

    edit actions

--- src/components/ProfileEditor.js
+++ src/components/ProfileEditor.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Script from 'react-load-script'
+import { connect } from 'react-redux'
 
 class ProfileEditor extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      first_name: props.user.first_name,
+      last_name: props.user.last_name,
+      locattion: props.user.location,
+      email: props.user.email,
+      avatar: props.user.avatar,
+      password: "",
+      passwordVerify: ""
 
     }
 
@@ -12,17 +20,40 @@ class ProfileEditor extends React.Component {
   }
 
   handleChange(e) {
-    this.setState([e.target.id]: e.target.value)
+    this.setState({[e.target.id]: e.target.value})
   }
 
-
-  render() {
+  get editForm() {
     return (
-      <div>
+      <div className="profile-editor">
+        <input type="text" id="first_name" onChange={e=>this.handleChange(e)} value={this.state.first_name}/>
+        <input type="text" id="last_name" onChange={e=>this.handleChange(e)} value={this.state.last_name}/>
+        <input type="text" id="email" onChange={e=>this.handleChange(e)} value={this.state.email}/>
+        <input type="text" id="location" onChange={e=>this.handleChange(e)} value={this.state.location}/>
+        <p>New Password</p>
+        <input type="text" id="password" onChange={e=>this.handleChange(e)} value={this.state.pasword}/>
+        <input type="text" id="passwordVerify" onChange={e=>this.handleChange(e)} value={this.state.passwordVerify}/>
       </div>
     )
   }
+
+  render() {
+    console.log(!!this.props.user.id)
+    return (
+    <>
+    {!!this.props.user.id ? this.editForm : null}
+    </>
+    )
+  }
 }
+
+// const mapStateToProps = state => {
+//   return {
+//     user: state.user.user
+//   }
+// }
+
+export default ProfileEditor
 
 
 //first last username email avatar location email password passwordVerify
--- src/containers/ProfileViewer.js
+++ src/containers/ProfileViewer.js
@@ -2,18 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Loader from '../components/loader'
 import ProfileWidget from '../components/ProfileWidget'
+import ProfileEditor from '../components/ProfileEditor'
 
 class ProfileViewer extends React.Component {
 
+  get profileInfo() {
+    if (this.props.user.user.id === undefined) {
+      return <Loader/>
+    }
+    else return (
+      <div className="profile-viewer">
+        <button>edit my profile</button>
+        <ProfileWidget/>
+        <ProfileEditor user={this.props.user.user}/>
+      </div>
+    )
+  }
 
   render() {
     console.log(this.props.user)
     return (
-      <div className="profile-viewer">
-        <button>edit my profile</button>
-        <ProfileWidget/>
-      </div>
 
+      <>
+      {this.profileInfo}
+      </>
     )
   }
 }
