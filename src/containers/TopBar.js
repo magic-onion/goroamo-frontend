cbc601cda9fd29c14b87529dc8d70f28bd524c22
magic-onion <alexander.m.truong@gmail.com>
2019-11-07 10:15:18 -0600

    breakpoints added

--- src/containers/TopBar.js
+++ src/containers/TopBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LogOutButton from '../components/logout'
 
 class TopBar extends React.Component {
+
+
 
   render() {
     return(
       <div className="top-bar">
           <img className="logo-img" src={require('../assets/logo-side.png')} alt="GoRoamo-logo"></img>
         <div className="top-bar-controls">
-          <span className="top-bar-profile">My Profile</span>
+          <Link to="/profile/" className="top-bar-profile">My Profile</Link>
           <LogOutButton/>
         </div>
       </div>
