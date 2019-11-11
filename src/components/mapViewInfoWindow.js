a5bd4b93aee5fa6f9420b51a01ba1e4296bf0b18
magic-onion <alexander.m.truong@gmail.com>
2019-11-11 17:13:54 -0600

    map view info window styling

--- src/components/mapViewInfoWindow.js
+++ src/components/mapViewInfoWindow.js
@@ -6,12 +6,14 @@ import { connect } from 'react-redux'
 class MapViewInfoWindow extends React.Component {
 
   render() {
+    console.log(this.props)
     let linkString = `/tours/${this.props.tour.tour.id}`
     return (
       <div className="map-view-info-window">
-        <p>Name: {this.props.tour.tour.name}</p>
-        <p>Locations: {this.props.tour.locations.length}</p>
-        <Link to={{pathname: linkString}}>View</Link>
+        <p className="map-view-info-window-tour-items">Name: {this.props.tour.tour.name}</p>
+        <p className="map-view-info-window-tour-items">Locations: {this.props.tour.locations.length}</p>
+        <p className="map-view-info-window-tour-items">Duration: {this.props.tour.tour.duration} min</p>
+        <Link className="map-view-info-window-tour-link" to={{pathname: linkString}}>View</Link>
       </div>
     )
   }
