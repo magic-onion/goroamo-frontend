af95dc53fbdfaa0fa2db56471f3fcf695712bc9a
magic-onion <alexander.m.truong@gmail.com>
2019-11-15 15:22:18 -0600

    mobile styling

--- src/containers/MobileSidebar.js
+++ src/containers/MobileSidebar.js
@@ -7,13 +7,18 @@ function MobileSidebar(props) {
       <div className="mobile-nav-container">
 
       <div className="mobile-item">
-        <img className="mobile-nav-img" src={require(`../assets/profile-icon.png`)} alt="hi"></img>
+        <img className="mobile-nav-img" src={require(`../assets/profile-icon.png`)} alt="profile"></img>
         <NavLink onClick={props.menuOff} exact to="/profile/" className="mobile-nav">Profile</NavLink>
       </div>
 
       <div className="mobile-item">
-        <img className="mobile-nav-img" src={require(`../assets/walking-icon.png`)} alt="hi"></img>
+        <img className="mobile-nav-img" src={require(`../assets/walking-icon.png`)} alt="view tours"></img>
         <NavLink onClick={props.menuOff} exact to="/view-tours/" className="mobile-nav">View Tours</NavLink>
+      </div>
+
+      <div className="mobile-item">
+        <img className="mobile-nav-img" src={require(`../assets/analytics-icon.png`)} alt="about"></img>
+        <NavLink onClick={props.menuOff} exact to="/about/" className="mobile-nav">About</NavLink>
       </div>
 
       </div>
